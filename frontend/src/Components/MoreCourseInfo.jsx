ee3b4a1327371a79c38f06e829e58a07e5725354
George Ekwemadu <70635322+TonyBllvc@users.noreply.github.com>
2023-08-10 16:50:15 -0400

    Update MoreCourseInfo.jsx

--- frontend/src/Components/MoreCourseInfo.jsx
+++ frontend/src/Components/MoreCourseInfo.jsx
@@ -4,13 +4,14 @@ import { FaTrashAlt } from 'react-icons/fa'
 import { useCourseContext } from '../hooks/useCourseContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 
+const baseURL = https://faithful-teal-bathing-suit.cyclic.app
 const MoreCourseInfo = ({ course }) => {
     const { dispatch } = useCourseContext()
     const { user } = useAuthContext()
 
     const handleDelete = async () => {
 
-        const response = await fetch('/api/course/' + course._id, {
+        const response = await fetch(`${baseURL}/api/course/` + course._id, {
             method: 'DELETE',
             headers: {
                 "Content-Type": "application/json",
