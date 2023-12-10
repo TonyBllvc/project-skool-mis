c145f5509a72acf5f3a02d534e9fcc1a6018a8c2
TonyBllvc <george.ekwemadu@gmail.com>
2023-12-10 15:24:13 +0100

    secret again

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose');
 
-var SECRET = 'makealrichtoirhbetherebowsermfjckove9rjftimebowser'
+// var SECRET = 'makealrichtoirhbetherebowsermfjckove9rjftimebowser'
 // to crease a web token 
 const createToken = (_id, role) => {
 
@@ -12,7 +12,7 @@ const createToken = (_id, role) => {
     //  1. the payload which is the {_id})
     // 2. the secret for just the server (stored on the '.env' file)
     // 3. any property -- this case, the expires property
-    return jwt.sign({ _id, role }, SECRET, { expiresIn: '1d' })
+    return jwt.sign({ _id, role },'makealrichtoirhbetherebowsermfjckove9rjftimebowser', { expiresIn: '1d' })
     // return jwt.sign({ _id, role }, process.env.SECRET, { expiresIn: '1d' })
 }
 
