46922d2d94879c990c00bae7919c3cbd2bae3369
TonyBllvc <george.ekwemadu@gmail.com>
2024-08-04 16:00:36 +0100

    student details change again

--- frontend/src/Components/StudentDetails.jsx
+++ frontend/src/Components/StudentDetails.jsx
@@ -4,32 +4,32 @@ import React, { useEffect, useState } from 'react'
 const StudentDetails = ({ studentInfo }) => {
 
   return (
-    <Tbody width={['65%', '75%', '75%' ]}>
-        <Tr display='flex' width='100%' flexDirection='column' backgroundColor='whiteAlpha.900'   >
-          <Td width='100%' py={2.5} display='flex' justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
-            <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
-              { studentInfo.surname} {studentInfo.first_name}
-            </Box>
-          </Td>
-          <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
-            <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
+    <Tbody width={['65%', '75%', '75%']}>
+      <Tr display='flex' width='100%' flexDirection='column' backgroundColor='whiteAlpha.900'   >
+        <Td width='100%' py={2.5} display='flex' justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
+          <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
+            {studentInfo.surname} {studentInfo.first_name}
+          </Box>
+        </Td>
+        <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
+          <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
             {/* { studentInfo.student_id.faculty} */}
-            { studentInfo.department}
-            </Box>
-          </Td>
-          <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
-            <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
-              {/* { studentInfo.student_id.department} */}
-              { studentInfo.faculty}              
-            </Box>
-          </Td>
-          <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
-            <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
-              {/* { studentInfo.student_id.reg_no} */}
-              { studentInfo.reg_no}
-            </Box>
-          </Td>
-        </Tr>
+            {studentInfo.faculty}
+          </Box>
+        </Td>
+        <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
+          <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
+            {/* { studentInfo.student_id.department} */}
+            {studentInfo.department}
+          </Box>
+        </Td>
+        <Td width='100%' py={2.5} justifyContent='start' fontSize={['8', '12', '16']} overflow='hidden' textOverflow='ellipsis' wordBreak='break-all'>
+          <Box width={['100%', '100%', '100%']} fontSize={['10', '11', '15', '17']} >
+            {/* { studentInfo.student_id.reg_no} */}
+            {studentInfo.reg_no}
+          </Box>
+        </Td>
+      </Tr>
     </Tbody>
   )
 }
